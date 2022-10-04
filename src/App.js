4c7262e4987afe906b997e40918420336d0ffe6d
Steve <steve.maina@student.moringaschool.com>
2022-10-04 21:44:41 +0300

    Add like button

--- src/App.js
+++ src/App.js
@@ -16,21 +16,39 @@ function App(){
     console.log(response.data)
     const data = await response.data;
     setImages(data)
+   
 
 
   }
+  const [count, setCount] = useState(0);
 
   return(
     <div className="container">
+      <h1>Blake Photography Voting</h1>
       <br></br>
       <button className="btn btn-primary btn-md">Get</button>
    
       <br></br>
       <div className="photos">
-      <img src="https://media.wired.com/photos/5fb70f2ce7b75db783b7012c/master/pass/Gear-Photos-597589287.jpg" className="im1"></img>
-      </div> 
+        <div className="p">
+     <img src="https://media.wired.com/photos/5fb70f2ce7b75db783b7012c/master/pass/Gear-Photos-597589287.jpg" className="im1">
+      
+     </img>
+     
+     {/* <button >
+        <span class="glyphicon">&#xe125;</span>
+        </button> */}
+          <div>
+      <p>{count} likes</p>
+      <button  onClick={() => setCount(count + 1)}>
+      <span class="glyphicon">&#xe125;</span>
+        
+      </button>
     </div>
-  )
+      </div> 
+      </div>
+    </div>
+  ) 
     
   }
 
