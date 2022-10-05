b0114d79569c4ab8f0576413a9f4c91dd27482e2
Steve <steve.maina@student.moringaschool.com>
2022-10-05 17:10:37 +0300

    add footer

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css';
+
+
+function Nav(){
+    return(
+        <div>
+            <ul className="Nav">
+                <li>Blake Photography</li>
+                <li>Home</li>
+                <li>Vote</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+    )
+}
+export default Nav;
