8d1f8a56b524d50c82a0e889769eea7179f8e082
Steve <steve.maina@student.moringaschool.com>
2022-10-05 17:50:58 +0300

    add imageCard

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,11 @@ import React from "react";
 function Footer(){
     return(
         <>
-       <ul>
+       <ul className="Footer">
         <li>Blake Photography Services</li>
+        <li>Location:Parliament Road Nairobi CBD</li>
+        <li>Contact Us:0112309874</li>
+       
        </ul>
 
 
