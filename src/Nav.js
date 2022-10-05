b9470e345b2f7b35de42760d4a35191f0555b4aa
Steve <steve.maina@student.moringaschool.com>
2022-10-06 01:59:29 +0300

    style nav

--- src/Nav.js
+++ src/Nav.js
@@ -7,7 +7,7 @@ function Nav(){
     return(
         <div>
             <ul className="Nav">
-                <li>Blake Photography App</li>
+                <li>Blake Photography Application</li>
                 <li>Home</li>
                 <li a href="./App">Vote</li>
                 <li>Contact Us</li>
