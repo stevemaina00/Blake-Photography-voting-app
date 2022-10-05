67e05d82e5f9da7c70af5d1ee19ed1f6a43cf187
Steve <steve.maina@student.moringaschool.com>
2022-10-05 21:23:02 +0300

    add Timer

---
+++ src/Timer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Timer(){
+    const dateWithTime = new Date().toLocaleString().
+    return(
+        <>
+        <p>{dateWithTime}</p>
+        </>
+    )
+}
+export default Timer;
