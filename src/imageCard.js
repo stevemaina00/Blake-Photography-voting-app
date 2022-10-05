8739589f81344a082500eb251f32f090548545c9
Steve <steve.maina@student.moringaschool.com>
2022-10-05 17:55:29 +0300

    add boxes

---
+++ src/imageCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+function imageCard(){
+    return(
+        <div>
+        <div>
+             <div className="place1">
+            
+            </div>
+            <div className="place2">
+            
+            </div>
+            <div className="place3">
+            
+            </div>
+
+        </div>
+        </div>
+       
+    )
+
+}
